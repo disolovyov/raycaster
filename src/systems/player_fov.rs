0b2601a37a181d2a39e45acf89ebeb02c3a8f886
Dimitry Solovyov <dimitry@solovyov.lv>
2019-07-30 22:56:35 +0300

    Clean up fov system a bit

--- src/systems/player_fov.rs
+++ src/systems/player_fov.rs
@@ -21,60 +21,66 @@ impl<'a> System<'a> for PlayerFovSystem {
     fn run(&mut self, data: Self::SystemData) {
         let (players, transforms, mut renderer, room) = data;
 
+        if let Some((_, transform)) = (&players, &transforms).join().next() {
+            let framebuffer = PlayerFovSystem::render(transform, &room);
+            renderer.add(RenderItem {
+                renderable: Renderable::Framebuffer(framebuffer),
+                position: Vector::ZERO,
+                layer: Layer::FOV,
+            })
+        }
+    }
+}
+
+impl PlayerFovSystem {
+    fn render(transform: &Transform, room: &Room) -> Framebuffer {
         let mut framebuffer = Framebuffer::new(VW, VH);
+        for ray in 0..VW {
+            let angle = transform.angle - FOV / 2.0 + FOV * ray as f32 / VW as f32;
 
-        for (_, transform) in (&players, &transforms).join() {
-            for ray in 0..VW {
-                let angle = transform.angle - FOV / 2.0 + FOV * ray as f32 / VW as f32;
+            for i in 0..2000 {
+                let t = i as f32 * 0.01;
+                let cx: f32 = transform.position.x + t * angle.cos();
+                let cy: f32 = transform.position.y + t * angle.sin();
 
-                for i in 0..2000 {
-                    let t = i as f32 * 0.01;
-                    let cx: f32 = transform.position.x + t * angle.cos();
-                    let cy: f32 = transform.position.y + t * angle.sin();
+                let tile = room.get(cx as u32, cy as u32);
+                if tile == 0 {
+                    continue;
+                }
 
-                    let tile = room.get(cx as u32, cy as u32);
-                    if tile != 0 {
-                        let mut column_height =
-                            (VW as f32 / (t * (angle - transform.angle).cos())) as u32;
-                        if column_height > VH {
-                            column_height = VH;
-                        }
+                let mut column_height = (VW as f32 / (t * (angle - transform.angle).cos())) as u32;
+                if column_height > VH {
+                    column_height = VH;
+                }
 
-                        let hit_x = cx - (cx + 0.5).floor();
-                        let hit_y = cy - (cy + 0.5).floor();
-                        let mut texture_x = match hit_y.abs() > hit_x.abs() {
-                            true => (hit_y * TILE_SIZE as f32) as i32,
-                            false => (hit_x * TILE_SIZE as f32) as i32,
-                        };
-                        if texture_x < 0 {
-                            texture_x += TILE_SIZE as i32;
-                        }
+                let texture_x = PlayerFovSystem::get_texture_x(cx, cy);
+                let texture_column =
+                    room.get_texture_column(tile, texture_x as usize, column_height as usize);
 
-                        let texture_column = room.get_texture_column(
-                            tile,
-                            texture_x as usize,
-                            column_height as usize,
-                        );
-
-                        let x = ray;
-                        let column_start = VH / 2 - column_height / 2;
-                        for column_y in 0..column_height {
-                            let y = column_start + column_y;
-                            if y < VH {
-                                framebuffer.draw_pixel(x, y, texture_column[column_y as usize])
-                            }
-                        }
-
-                        break;
+                let column_start = VH / 2 - column_height / 2;
+                for column_y in 0..column_height {
+                    let y = column_start + column_y;
+                    if y < VH {
+                        framebuffer.draw_pixel(ray, y, texture_column[column_y as usize])
                     }
                 }
+
+                break;
             }
         }
+        framebuffer
+    }
 
-        renderer.add(RenderItem {
-            renderable: Renderable::Framebuffer(framebuffer),
-            position: Vector::ZERO,
-            layer: Layer::FOV,
-        })
+    fn get_texture_x(cx: f32, cy: f32) -> i32 {
+        let hit_x = cx - (cx + 0.5).floor();
+        let hit_y = cy - (cy + 0.5).floor();
+        let mut texture_x = match hit_y.abs() > hit_x.abs() {
+            true => (hit_y * TILE_SIZE as f32) as i32,
+            false => (hit_x * TILE_SIZE as f32) as i32,
+        };
+        if texture_x < 0 {
+            texture_x += TILE_SIZE as i32;
+        }
+        texture_x
     }
 }
